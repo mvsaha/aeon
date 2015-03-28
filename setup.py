9b3b2ea50d024bfb3fc19a84b83188189f1983ef
mvsaha <michael.saha@gmail.com>
2015-03-28 17:59:36 -0400

    setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,16 @@
 # setup.py
 
+from distutils.core import setup
+
 config = {
     'description': 'eon',
     'author': 'Michael Saha',
     'url': 'https://github.com/mvsaha/eon',
     'download_url': 'https://github.com/mvsaha/eon',
     'author_email': 'https://github.com/mvsaha/eon',
-    'version': '0.1',
+    'version': '0.1.0',
     'install_requires': [],
     'packages': [],
-    'scripts': [],
     'name': 'eon'
 }
 
