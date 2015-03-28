a0bc961cad7e8b9a61e6054d5aab89d1999e4bcf
mvsaha <michael.saha@gmail.com>
2015-03-28 17:34:40 -0400

    Restructured directories

---
+++ setup.py
@@ -0,0 +1,16 @@
+# setup.py
+
+config = {
+    'description': 'eon',
+    'author': 'Michael Saha',
+    'url': 'https://github.com/mvsaha/eon',
+    'download_url': 'https://github.com/mvsaha/eon',
+    'author_email': 'https://github.com/mvsaha/eon',
+    'version': '0.1',
+    'install_requires': [],
+    'packages': [],
+    'scripts': [],
+    'name': 'eon'
+}
+
+setup(**config)
